5fde7f00b4aa9ec996081daac5eb68fe186ebc85
Jacob Killelea <Jkillelea@protonmail.ch>
2017-03-21 10:38:25 -0600

    better output

--- main.m
+++ main.m
@@ -18,14 +18,17 @@ mat = compare_genres([pg_movies.Action, pg_movies.Drama, pg_movies.Romance]);
 
 [vecs, vals] = eig(mat);
 [val, idx]   = max(max(vals));    % extract largest Eigenvalue from matrix (flatten, find max)
-
-fprintf('max Eigenvalue: %f.\n', val);
-
 vec          = vecs(:, idx);       % extract largest Eigenvector
 [~,   idxs]   = sort(vec);         % sort the Eigenvector to get indexes sorted (we care about the last one)
 idxs          = idxs((end-2):end); % pull out the last three indexes
 
-% fprintf('best movie at position %d.\n', idx);
-for i = idxs
-  fprintf('%s.\n', pg_movies.title{i});
+% print out results to console and file
+fid = fopen('output.txt', 'w');
+for id = [1, fid]
+  fprintf(id, 'max Eigenvalue: %f.\n', val);
+  for i = idxs
+    fprintf(id, '%s.\n', pg_movies.title{i});
+  end
+  fprintf(id, 'best: %s.\n', pg_movies.title{idxs(end)});
 end
+fclose(fid);
